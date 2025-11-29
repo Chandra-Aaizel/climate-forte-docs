abe4ba3f204dd0904850759339cb3464bfcca083
Chandra-Aaizel <chandra@aaizeltech.com>
2025-11-29 14:32:01 +0530

    after changes

--- app/_meta.js
+++ app/_meta.js
@@ -3,7 +3,6 @@ const metaData = {
         type :"separator",
         title : "Fundamentals"
     },
-<<<<<<< HEAD
     "docs" : "Climate Forte Docs",
     "weather_data_layers":"Weather Data Layers",
     "access_keys" : "Access Key",
@@ -13,12 +12,5 @@ const metaData = {
     "error_handling": "Error Handling",
     "tools_library": "Tools & Library",
     "release_notes":"Release Notes",
-=======
-    docs : "Climate Forte APIs",
-    access : "Access Key",
-    versioning : "Versioning",
-    errorHandling: "Error Handling",
-    toolslibrary: "Tools & Library"
->>>>>>> d86a3d6da063ab9065e0fce36d180547d6e631a2
 }
 export default metaData;
